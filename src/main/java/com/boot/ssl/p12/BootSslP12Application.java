c7b5ed704ceeffae0b4914d527b0f41a254ca7b1
ratna1992 <ratnasrinivas1992@gmail.com>
2023-01-28 10:31:45 +0530

    first commit

---
+++ src/main/java/com/boot/ssl/p12/BootSslP12Application.java
@@ -0,0 +1,13 @@
+package com.boot.ssl.p12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSslP12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSslP12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/ssl/p12/BootSslP12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.ssl.p12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSslP12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
